26c28f32a0aa34969c7dcf235aa29493cd0467fa
qwwqe <f.werth@gmail.com>
2023-10-07 19:38:22 +0800

    Correct typos

--- src/game/player.go
+++ src/game/player.go
@@ -4,8 +4,8 @@ import "github.com/qwwqe/demesne/src/card"
 
 // A Player represents a participant in a game of Demesne.
 // It does not attempt to implement any functionality related to the identity of
-// or communciation between the actual humans or programs controlling such participation.
-// In other words, notions such as names, registration, friends, and the like are strictly
+// or communication between the actual humans or programs controlling such participation.
+// In other words, notions such as names, user registration, and the like are strictly
 // outside the domain served by this type.
 //
 // TODO: Determine how Stage-specific state, such as progress in a given turn,
