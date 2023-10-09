02eb37060842719f2b42c5eb205f301a93a21445
qwwqe <f.werth@gmail.com>
2023-10-09 15:10:28 +0800

    Define basic CardSet

--- src/game/ruleset.go
+++ src/game/ruleset.go
@@ -1,6 +1,14 @@
 package game
 
-// A Ruleset defines how a game of Demesne is set up
+import "github.com/qwwqe/demesne/src/card"
+
+// A RuleSet defines how a game of Demesne is set up
 // and how end game is determined, among other things.
-type Ruleset struct {
+type RuleSet struct {
+	CardSets []CardSet
+}
+
+type CardSet interface {
+	Card() card.Card
+	Amount(players int) int
 }
