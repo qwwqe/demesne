99948692a1edc11efd3c0fd7d6c87e39043b31e1
qwwqe <f.werth@gmail.com>
2023-10-29 20:02:48 +0800

    Redefine EndCondition as a interface backed by fixed-structure conditions

---
+++ src/game/endcondition.go
@@ -0,0 +1,52 @@
+package game
+
+// EndCondition describes end conditions for a game.
+type EndCondition interface {
+	// Evaluate determines whether end conditions have been met.
+	Evaluate(game) bool
+}
+
+// EmptySupplyEndCondition is an end condition that looks at the
+// number of empty piles in the supply.
+type EmptySupplyEndCondition struct {
+	Count int
+}
+
+// Evaluate determines whether the end condition has been met.
+func (ec EmptySupplyEndCondition) Evaluate(g game) bool {
+	empty := 0
+
+	for _, pile := range g.Supply.All() {
+		if pile.Size() == 0 {
+			empty++
+		}
+
+		if empty >= ec.Count {
+			return true
+		}
+	}
+
+	return false
+}
+
+var _ EndCondition = EmptySupplyEndCondition{}
+
+// EmptyPileEndCondition is an end condition that looks at the
+// size of a specific pile.
+type EmptyPileEndCondition struct {
+	Id string
+}
+
+// Evaluate determines whether the end condition has been met.
+func (ec EmptyPileEndCondition) Evaluate(g game) bool {
+	// NOTE: Should a Game be maintaining a lookup so that
+	// checks like this don't need to keep iterating over
+	// all the piles?
+	for _, pile := range g.Supply.All() {
+		if pile.Id == ec.Id {
+			return pile.Size() == 0
+		}
+	}
+
+	return false
+}
