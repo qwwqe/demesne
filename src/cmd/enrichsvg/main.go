17de1f3595d208f9c8c735e1e705596ef9554d0a
qwwqe <f.werth@gmail.com>
2023-11-05 22:05:43 +0800

    Stub out potential SVG post-processor (though using XLST seems to work fine for now)

--- src/cmd/enrichsvg/main.go
+++ src/cmd/enrichsvg/main.go
@@ -1,8 +1,17 @@
+// Stub for post-processing SVGs output by PlantUML.
+// Not entirely clear on
 package main
 
 import (
-	"fmt"
+	"encoding/xml"
 )
+
+type XMLNode struct {
+	XMLName xml.Name
+	Attrs []xml.Attr `xml:",any,attr"`
+	Content []byte `xml:",innerxml"`
+	XMLNodes []XMLNode `xml:",any"`
+}
 
 func main() {
 
