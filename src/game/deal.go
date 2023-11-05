b9a41e910ef001a1b47e7f640694c034e505591f
qwwqe <f.werth@gmail.com>
2023-11-05 23:34:43 +0800

    Define basic Deal mechanism

---
+++ src/game/deal.go
@@ -0,0 +1,39 @@
+package game
+
+type Dealer interface {
+	// Deal a single hand.
+	Deal(p *Pile) []Card
+}
+
+// DestructiveDealer deals by directly drawing from a pile.
+type DestructiveDealer struct {
+	amount int
+}
+
+// Deal a single hand.
+func (d DestructiveDealer) Deal(p *Pile) []Card {
+	return p.Draw(d.amount)
+}
+
+var _ Dealer = DestructiveDealer{}
+
+// NonDestructiveDealer deals by cloning the top card of a pile.
+type NonDestructiveDealer struct {
+	amount int
+}
+
+// Deal a single hand.
+func (d NonDestructiveDealer) Deal(p *Pile) []Card {
+	card := p.Top()
+	if card == nil {
+		return nil
+	}
+
+	cs := make([]Card, d.amount)
+	for i := 0; i < len(cs); i++ {
+		cs[i] = card.Clone()
+	}
+	return cs
+}
+
+var _ Dealer = NonDestructiveDealer{}
